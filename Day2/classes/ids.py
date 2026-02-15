76564745b048ca5fdde5405490cf70fc7973cd51
Daniel Lashyn <qch849@usask.ca>
2026-02-15 17:07:24 -0600

    Day2: Able to set the first id

--- Day2/classes/ids.py
+++ Day2/classes/ids.py
@@ -1,8 +1,21 @@
 class ids:
     def __init__(self, idRanges):
-        placeholder = "placeholder"
+        self.setStartid(idRanges)
+
+    def setStartid(self, passID):
+
+        if isinstance(passID, str) and passID.isdigit():
+            self.startID = int(passID)
+
+        elif isinstance(passID, int):
+            self.startID = passID
+
+        else:
+            print("Error: Not valid start id")
+            self.startID = 0
 
 
 
     def display(self):
-        print("Placeholder text for the ids class")
+        print("Starting ID: " + str(self.startID))
+
--- Day2/main.py
+++ Day2/main.py
@@ -1,3 +1,8 @@
+from classes.ids import ids
 
 
+idtest = ids("50")
+
+
+idtest.display()
 print("main")
