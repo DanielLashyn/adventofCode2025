d4e868ff2d4be4c32fe6fbf200d59159f288ed42
Daniel Lashyn <qch849@usask.ca>
2026-02-15 18:34:37 -0600

    Day2: updated to take the input on one line

--- Day2/main.py
+++ Day2/main.py
@@ -9,7 +9,8 @@ print("Using Data from " + data_file)
 
 # Gets the raw data from the text file
 with open(data_file, "r") as file:
-    rawData = file.read().splitlines()
+    rawData = file.readline().rstrip("\n")
+    rawData = rawData.split(',')
 
 idList = [ids(data) for data in rawData]
 total = sum(ids.sumIDs() for ids in idList)
