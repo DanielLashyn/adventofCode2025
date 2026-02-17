a71f7a034cc4d00d8105b57c1c16a5ce812303ec
Daniel Lashyn <qch849@usask.ca>
2026-02-17 17:41:34 -0600

    Day2: Added loop to advance ids

--- Day2/classes/advanceIDs.py
+++ Day2/classes/advanceIDs.py
@@ -3,4 +3,15 @@ from .ids import ids
 class advanceIDs(ids):
 
     def _setInvalidID(self):
-        print("TODO")        
+        self.invalidIDs = []
+        startLength = len(str(self.startID))
+        endLength = len(str(self.startID))
+        self.invalidIDs.append(endLength)
+        
+        for curLength in range(1, endLength // 2 + 1):
+            self.invalidIDs.append(curLength)   
+            #middle = len(curStr)//2
+            #startHalf = curStr[0:middle]
+            #endHalf = curStr[middle:]
+
+
