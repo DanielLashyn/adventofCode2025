0b0c5cc5ffce76a402a05441a58f16f8219fdb8a
Daniel Lashyn <qch849@usask.ca>
2026-02-17 17:50:59 -0600

    Day2: Add check to skip itertation if not able curLength doesn't work with range

--- Day2/classes/advanceIDs.py
+++ Day2/classes/advanceIDs.py
@@ -9,7 +9,19 @@ class advanceIDs(ids):
         self.invalidIDs.append(endLength)
         
         for curLength in range(1, endLength // 2 + 1):
-            self.invalidIDs.append(curLength)   
+            
+            # Checks if it's valid to use current length
+            if ((startLength - endLength) < 2 and
+                    startLength % curLength!= 0 and
+                    endLength % curLength != 0):
+                continue
+
+            self.invalidIDs.append(curLength)
+
+
+
+
+
             #middle = len(curStr)//2
             #startHalf = curStr[0:middle]
             #endHalf = curStr[middle:]
