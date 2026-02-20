7d91749ece8693410438bec01b9a6543a9bf7988
Daniel Lashyn <qch849@usask.ca>
2026-02-19 21:46:54 -0600

    main: Cleaned up code

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from Day2.main import * 
-from globalCode.classDayTemplate import DayTemplate
-from globalCode.headerFileReader import *
-'''
-test = DayTemplate(inputFileReader = fileReaderOneLine())
 
-test.displayIntro()
-test.setData()
-test.displayResult()
-'''
 tester = Day2(inputFileName = "input_real.txt", 
         inputDifficulty = Diff.ADVANCE)
 tester.run()
