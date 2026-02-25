a82b36df57f81fa6bb835d5c0218cba1b3d6af52
Daniel Lashyn <qch849@usask.ca>
2026-02-24 19:06:58 -0600

    globalCode: started to add code to display section of the data over a range

--- globalCode/classGridMap.py
+++ globalCode/classGridMap.py
@@ -1,5 +1,10 @@
 class GridMap:
     def __init__(self, gridData):
+        
+        self.gridData = []
+        self.row = 0
+        self.column = 0
+
         self.setGridData(gridData)
 
     def setGridData(self, gridData):
@@ -38,7 +43,7 @@ class GridMap:
     def display(self):
         
         if not self.gridData:
-            print("There is no Grid Mao to display!")
+            print("There is no Grid Map to display!")
             return
 
         # Prints the column header
@@ -54,11 +59,42 @@ class GridMap:
             for data in rowData:
                 print(" "+ str(data), end = "")
 
-
             print()
 
+    def displaySection(self, rowRange, columnRange):
+        
+        # Checks that the ranges are valid
+        if not self._isValidRange(rowRange, self.row):
+           #not self._isValidRange(columnRange, self.column):
+            print("Unable to Display!")
+            return
+
+        data = self.gridData[columnRange[0]:columnRange[1]][rowRange[0]:rowRange[1]])
+        
+    def _isValidRange(self, inRange, maxRange):
+        
+        if not isinstance(inRange, tuple):
+            print("GridMap Error: Range is not a tuple!")
+            return False
+
+        if len(inRange) != 2:
+            print("GridMap Error: Range is not exactly 2 values!")
+            return False
+
+        if not all(isinstance(i, int) for i in inRange):
+            print("GridMap Error: Range is not integer")
+            return False
+
+        if not all( (i >= 0 and i < maxRange) for i in inRange):
+            print("GridMap Error: is not a valid range")
+            return False
+
+        #if not all(inRange[0] > inRange[1]):
+            #print("GridMap Error: is not a valid range")
+            #return False
+
+
+        return True
 
 
 
-
-           
