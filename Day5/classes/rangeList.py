98dd4d64340f81ab087ba6e8fa2b23d74078a35d
Daniel Lashyn <qch849@usask.ca>
2026-04-19 22:30:43 -0600

    Day5: added list class with common methods

---
+++ Day5/classes/rangeList.py
@@ -0,0 +1,106 @@
+
+
+class rangeList(GridMap):
+    
+    # Assume ranges in the index are greater then or equal to 0
+    def __init__(self):
+        binaryList = []
+
+    # Returns True if the list is empty, otherwise False
+    def isEmpty(self):
+        if self.getLength() = -1:
+            return True
+        return False
+
+    # Gets the list length
+    def getLength(self):
+        return len(binaryList) - 1
+
+    # Gets the ranges at the selected index
+    def getRangeAtIndex(self, index):
+       if not self._validIndex(self, index):
+           return (-1, -1)
+       return binaryList[index]
+
+    # Gets the max Range at the selected index
+    def getMaxAtIndex(self, index):
+        selRange = self.getRangeAtIndex(index)
+        
+        return selRange[1]
+
+    # Gets the min Range at the selected index
+    def getMinAtIndex(self, index):
+        selRange = self.getRangeAtIndex(index)
+        
+        return selRange[0]
+
+
+    # Adds
+    def addRange(self, newRange):
+        print("Stub")
+
+
+    # Checks that the passed index is valid within the list of ranges
+    def _validIndex(self, index):
+        
+        if self.isEmpty():
+            return False
+        if self.getLength() < index:
+            return False
+        if index < 0:
+            return False
+
+    # Return True if the range is valid, else False
+    def _validRange(self, inRange):
+
+        minRange = inRange[0]
+        maxRange = inRange[1]
+
+
+        # Checks that the range is int
+        if not isinstance(minRange, int):
+            return False
+        
+        if not isinstance(maxRange, int):
+            return False
+
+        if minRange < 0:
+            return False
+
+        if minRange > maxRange:
+            return False
+
+        return True
+
+
+    # Method to insert a new range at an index
+    # Returns True if able to insert at the index, otherwise False
+    def _insertRange(self, insertIndex, newRange):
+
+        # Checks that range is valid
+        if not self._validRange(newRange):
+            return False
+
+        # Checks that the index is valid
+        if not self._validIndex(insertIndex):
+            return False
+
+        binaryList.insert(insertIndex, newRange)
+        return True
+
+
+    # Method to pop range at an index
+    # Returns True if able to pop at the index, otherwise False
+    def _popRange(self, popIndex):
+
+        # Checks that index is valid
+        if not self._validIndex(popIndex):
+            return False
+
+        binaryList.pop(popIndex)
+        return True
+
+
+
+
+
