900ed43ba6971a67724db78ed7fd25601d54edf0
Daniel Lashyn <qch849@usask.ca>
2026-04-25 14:21:43 -0600

    Day 5: Added binary search

--- Day5/classes/rangeList.py
+++ Day5/classes/rangeList.py
@@ -7,18 +7,19 @@ class rangeList():
 
     # Returns True if the list is empty, otherwise False
     def isEmpty(self):
-        if self.getLength() == 0:
+        if self.getLength() == -1:
             return True
         return False
 
     # Gets the list length
     def getLength(self):
-        return len(self.binaryList)
+        return len(self.binaryList) - 1
 
     # Gets the ranges at the selected index
     def getRangeAtIndex(self, index):
-       if not self._validIndex(self, index):
+       if not self._validIndex(index):
            return (-1, -1)
+       
        return self.binaryList[index]
 
     # Gets the max Range at the selected index
@@ -45,22 +46,38 @@ class rangeList():
         if not self._validRange(newRange):
             return False
 
+        if endIndex == -1:
+            self.binaryList.append(newRange)
+            return True
+        
+        
         # TODO Add Binary list search here
+        while startIndex <= endIndex:
+            mid = (endIndex + startIndex) // 2
 
-        self._insertRange(0, newRange)
+            if self.getMaxAtIndex(mid) < newRange[0]:
+                print(str(self.getMaxAtIndex(mid)) + " < " + str(newRange[0]))
+                startIndex = mid + 1
+            else:
+                print(str(self.getMaxAtIndex(mid)) + " > " + str(newRange[0]))
+                
+                endIndex = mid - 1
+        
+        print(startIndex)
+        self._insertRange(startIndex, newRange)
 
 
     # Checks that the passed index is valid within the list of ranges
     def _validIndex(self, index):
             
         # Special condition for empty list
-        if self.isEmpty() and index == 0:
-            return True
+        if self.isEmpty():
+            return False
 
         # checks that the index is not greater then the length
         if self.getLength() < index:
-            
             return False
+
         # Checks that index is not less then 0
         if index < 0:
             return False
@@ -98,8 +115,8 @@ class rangeList():
             return False
            
         # Checks that the index is valid
-        if not self._validIndex(insertIndex):
-            return False
+        #if not self._validIndex(insertIndex):
+            #return False
         
         self.binaryList.insert(insertIndex, newRange)
         return True
