33e0a63a814b56fba3a0920bc4ff5443788d472b
Daniel Lashyn <qch849@usask.ca>
2026-05-17 17:43:25 -0600

    Day6: Init

---
+++ Day6/main.py
@@ -0,0 +1,23 @@
+from globalCode.headerAll import *
+
+class Day6(DayTemplate):
+
+    def __init__(self,
+                inputFileName = "input_real.txt",
+                inputDifficulty = Diff.NORMAL):
+        
+
+        super().__init__(inputDay = 6, 
+                    inputFileName = inputFileName,
+                    inputFileReader = fileReaderMultiLine(),
+                    inputDifficulty = inputDifficulty)
+
+    def run(self):
+        super().run()
+
+        print(self.getData())
+        self.result = 0
+
+
+        self.displayResult()
+
