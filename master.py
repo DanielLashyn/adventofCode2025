9f7a901021f0d1382fac10107e7ed78ef6caaead
Daniel Lashyn <qch849@usask.ca>
2026-05-18 11:39:19 -0600

    Day 6: using real data

--- master.py
+++ master.py
@@ -5,7 +5,7 @@ from Day4.main import *
 from Day5.main import *
 from Day6.main import *
 
-tester = Day6(inputFileName = "input_example.txt", 
+tester = Day6(inputFileName = "input_real.txt", 
         inputDifficulty = Diff.NORMAL)
 tester.run()
 
