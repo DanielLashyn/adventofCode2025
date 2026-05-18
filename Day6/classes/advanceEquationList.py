fbd51ffcdad8035f13856612914b7b3b2d5c534d
Daniel Lashyn <qch849@usask.ca>
2026-05-18 12:50:27 -0600

    Day 6: created method logic for creating the equation

--- Day6/classes/advanceEquationList.py
+++ Day6/classes/advanceEquationList.py
@@ -4,3 +4,26 @@ class advanceEquationList(equationList):
 
     def __init__(self):
         super().__init__()
+
+
+    # advance version of init Data
+    def initData(self, data):
+
+        dataLength = len(data)
+        columnLength = self.getDataLength(data)
+
+        for column in range (columnLength - 1, -1, -1):
+            equationStr = ""
+            for row in range (0, dataLength - 1):
+                temp = data[row]
+                equationStr = equationStr + str(temp[column])
+            print(equationStr)
+
+    def getDataLength(self, data):
+
+        maxValue = 0
+        for i in range(0, len(data) - 1):
+            maxValue = max(maxValue, len(data[1]))
+
+        return maxValue
+
