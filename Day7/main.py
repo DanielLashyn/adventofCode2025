1a06593149f5e9bfc78b59af4953dd6a4b7cbfdc
Daniel Lashyn <qch849@usask.ca>
2026-06-01 13:48:07 -0600

    Day7: init Commit

---
+++ Day7/main.py
@@ -0,0 +1,21 @@
+from globalCode.headerAll import *
+
+class Day7(DayTemplate):
+
+    def __init__(self,
+                inputFileName = "input_real.txt",
+                inputDifficulty = Diff.NORMAL):
+        
+
+        super().__init__(inputDay = 7, 
+                    inputFileName = inputFileName,
+                    inputFileReader = fileReaderMultiLine(),
+                    inputDifficulty = inputDifficulty)
+
+    def run(self):
+        super().run()
+
+        print(self.getData())
+        self.result = 0
+        self.displayResult()
+
--- master.py
+++ master.py
@@ -4,8 +4,9 @@ from Day3.main import *
 from Day4.main import *
 from Day5.main import *
 from Day6.main import *
+from Day7.main import *
 
-tester = Day6(inputFileName = "input_real.txt", 
+tester = Day7(inputFileName = "input_example.txt", 
         inputDifficulty = Diff.ADVANCE)
 tester.run()
 
